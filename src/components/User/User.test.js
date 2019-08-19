eb5ad148b545058de5d09dd4f1cc19f7fc448996
Mohamed Cherif <cherif.mohamed86@gmail.com>
2019-08-19 01:22:18 +0100

    new tests using enzym

--- src/components/User/User.test.js
+++ src/components/User/User.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {shallow, mount} from '../../enzyme';
 
 import User from './User';
+import App from '../../App';
 
 const users = [{
   "id": "5d552d0058f193f2795fc814",
@@ -30,24 +32,49 @@ it('renders correctly when there is one user', () => {
   expect(tree).toMatchSnapshot();
  });
 
-it('when the follow button is clicked a callback is executed', () => {
-  const mockFollowClick = jest.fn();
-  const mockStarClick = jest.fn();
+ describe('App Component', () => {
+  it('calling method from App Component has the expected effect on the state', () => {
+    AppComponent = shallow(<App />)
+    const wrapper = AppComponent.instance();
+    wrapper.clickFollowHandler('5d552d0058f193f2795fc814'); //mock maybe
+    expect(wrapper.state).toEqual({
+      "id": "5d552d0058f193f2795fc814",
+      "isFollowed": "idle",
+      "isStared": "idle",
+      "image": "./assets/images/avata.png",
+      "readingTime": 20,
+      "name": "Walton Morton",
+      "date": "Aug 9"
+    })
+  })
+});
+describe('User Component', () => {
+  it('clicking on the button will trigger the click handler', () => {
+    const mockHandler = jest.fn();
+    wrapper = mount(<User users={users} handleFollowClick={mockHandler} handleStarClick={mockHandler} />)
+    wrapper.find('button').simulate('click');
+    expect(mockHandler).toHaveBeenCalled();
+  })
+});
 
-  const tree = renderer.create(<User users={users}
-      handleFollowClick={mockFollowClick}
-      handleStarClick={mockStarClick}
-  />)
+// it('when the follow button is clicked a callback is executed', () => {
+//   const mockFollowClick = jest.fn();
+//   const mockStarClick = jest.fn();
 
-  const button = tree.root.findByType('button');
-  const input = tree.root.findByType('input');
+//   const tree = renderer.create(<User users={users}
+//       handleFollowClick={mockFollowClick}
+//       handleStarClick={mockStarClick}
+//   />)
 
-  button.props.onClick();
-  expect(mockFollowClick).toHaveBeenCalled();
+//   const button = tree.root.findByType('button');
+//   const input = tree.root.findByType('input');
 
-  // button.props.onClick();
-  // expect(mockFollowClick).toHaveBeenCalledWith('5d552d0058f193f2795fc814');
+//   button.props.onClick();
+//   expect(mockFollowClick).toHaveBeenCalled();
 
-  input.props.onChange();
-  expect(mockStarClick).toHaveBeenCalled();
-})
+//   button.props.onClick();
+//   expect(mockFollowClick).toHaveBeenCalledWith('5d552d0058f193f2795fc814');
+
+//   input.props.onChange();
+//   expect(mockStarClick).toHaveBeenCalled();
+// })
