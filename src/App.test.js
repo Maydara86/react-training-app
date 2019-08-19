4e165dcf41afe51585022b4d6c8d210a00b6ee87
Mohamed Cherif <cherif.mohamed86@gmail.com>
2019-08-19 15:47:11 +0100

    App component test succesful

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {shallow, mount} from './enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+jest.mock('./data/users-data.json')
+let {user} = require('./data/users-data.json')
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+describe('App Component', () => {
+  it('calling the clickFollowHandler method from App Component has the expected effect on the state of the first user', () => {
+    const AppComponent = shallow(<App />)
+    const wrapper = AppComponent.instance()
+    wrapper.clickFollowHandler('5d552d0058f193f2795fc814')
+    expect(wrapper.state.users[0].isFollowed).toEqual('idle')
+  })
+})
+
+
+
+
+describe('App Component', () => {
+  it('calling the clickStarHandler method from App Component has the expected effect on the state of the second user', () => {
+    const AppComponent = shallow(<App />)
+    const wrapper = AppComponent.instance()
+    wrapper.clickStarHandler('5d552d00b20b141dff10d2a2')
+    expect(wrapper.state.users[1].isStared).toEqual('idle')
+  })
+})
