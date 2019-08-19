0f522e2f5a4d638876ade98a300a9798604d1376
Mohamed Cherif <cherif.mohamed86@gmail.com>
2019-08-19 11:29:11 +0100

    User test working

--- src/components/User/User.test.js
+++ src/components/User/User.test.js
@@ -3,9 +3,8 @@ import renderer from 'react-test-renderer';
 import {shallow, mount} from '../../enzyme';
 
 import User from './User';
-import App from '../../App';
 
-const users = [{
+const users = {
   "id": "5d552d0058f193f2795fc814",
   "isFollowed": "active",
   "isStared": "idle",
@@ -13,68 +12,106 @@ const users = [{
   "readingTime": 20,
   "name": "Walton Morton",
   "date": "Aug 9"
-}];
+};
 
 it('renders correctly when there are no users', () => {
   const tree = renderer.create(<User 
-    users={[]} 
     handleFollowClick={() => 'test'}
-    handleStarClick={() => {}} />).toJSON();
+    handleStarClick={() => {}} 
+  />).toJSON();
   expect(tree).toMatchSnapshot();
 });
 
 it('renders correctly when there is one user', () => {
 
-  const tree = renderer.create(<User users={users}
+  const tree = renderer.create(<User 
+    key={users.id}
+    id={users.id}
+    name={users.name}
+    date={users.date}
+    readingTime={users.readingTime}
+    isStared={users.isStared}
+    isFollowed={users.isFollowed}
+    image={users.image}
     handleFollowClick={() => 'test'}
     handleStarClick={() => {}}
-     />).toJSON();
+  />).toJSON();
   expect(tree).toMatchSnapshot();
- });
-
- describe('App Component', () => {
-  it('calling method from App Component has the expected effect on the state', () => {
-    AppComponent = shallow(<App />)
-    const wrapper = AppComponent.instance();
-    wrapper.clickFollowHandler('5d552d0058f193f2795fc814'); //mock maybe
-    expect(wrapper.state).toEqual({
-      "id": "5d552d0058f193f2795fc814",
-      "isFollowed": "idle",
-      "isStared": "idle",
-      "image": "./assets/images/avata.png",
-      "readingTime": 20,
-      "name": "Walton Morton",
-      "date": "Aug 9"
-    })
-  })
 });
+
+
+
+
+it('when the follow button is clicked a callback is executed', () => {
+  const mockFollowClick = jest.fn();
+  const mockStarClick = jest.fn();
+
+  const tree = renderer.create(<User 
+    key={users.id}
+    id={users.id}
+    name={users.name}
+    date={users.date}
+    readingTime={users.readingTime}
+    isStared={users.isStared}
+    isFollowed={users.isFollowed}
+    image={users.image}
+    handleFollowClick={mockFollowClick}
+    handleStarClick={mockStarClick}
+  />)
+
+  const button = tree.root.findByType('button');
+  const input = tree.root.findByType('input');
+
+  button.props.onClick();
+  expect(mockFollowClick).toHaveBeenCalled();
+
+  button.props.onClick();
+  expect(mockFollowClick).toHaveBeenCalledWith('5d552d0058f193f2795fc814');
+
+  input.props.onChange();
+  expect(mockStarClick).toHaveBeenCalled();
+})
+
+
+
+
+
 describe('User Component', () => {
   it('clicking on the button will trigger the click handler', () => {
-    const mockHandler = jest.fn();
-    wrapper = mount(<User users={users} handleFollowClick={mockHandler} handleStarClick={mockHandler} />)
+    const mockFollowHandler = jest.fn();
+    const mockStarHandler = jest.fn();
+    const wrapper = mount(<User 
+      key={users.id}
+      id={users.id}
+      name={users.name}
+      date={users.date}
+      readingTime={users.readingTime}
+      isStared={users.isStared}
+      isFollowed={users.isFollowed}
+      image={users.image}
+      handleFollowClick={mockFollowHandler} 
+      handleStarClick={mockStarHandler} 
+    />)
     wrapper.find('button').simulate('click');
-    expect(mockHandler).toHaveBeenCalled();
+    expect(mockFollowHandler).toHaveBeenCalledWith('5d552d0058f193f2795fc814')
+  })
+
+  it('changing the star checkbox will trigger an onChange handler', () => {
+    const mockFollowHandler = jest.fn();
+    const mockStarHandler = jest.fn();
+    const wrapper = mount(<User 
+      key={users.id}
+      id={users.id}
+      name={users.name}
+      date={users.date}
+      readingTime={users.readingTime}
+      isStared={users.isStared}
+      isFollowed={users.isFollowed}
+      image={users.image}
+      handleFollowClick={mockFollowHandler} 
+      handleStarClick={mockStarHandler} 
+    />)
+    wrapper.find('input').simulate('change');
+    expect(mockStarHandler).toHaveBeenCalledWith('5d552d0058f193f2795fc814');
   })
 });
-
-// it('when the follow button is clicked a callback is executed', () => {
-//   const mockFollowClick = jest.fn();
-//   const mockStarClick = jest.fn();
-
-//   const tree = renderer.create(<User users={users}
-//       handleFollowClick={mockFollowClick}
-//       handleStarClick={mockStarClick}
-//   />)
-
-//   const button = tree.root.findByType('button');
-//   const input = tree.root.findByType('input');
-
-//   button.props.onClick();
-//   expect(mockFollowClick).toHaveBeenCalled();
-
-//   button.props.onClick();
-//   expect(mockFollowClick).toHaveBeenCalledWith('5d552d0058f193f2795fc814');
-
-//   input.props.onChange();
-//   expect(mockStarClick).toHaveBeenCalled();
-// })
