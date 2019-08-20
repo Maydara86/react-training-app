ed5cc22ca3b113b5b8326ff8f4615df63cb627ab
Unknown <cherif.mohamed86@gmail.com>
2019-08-20 10:08:20 +0100

    Adding felx containers

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -7,20 +7,22 @@ import PropTypes from 'prop-types'
 const User = props => {
   
     return(
-      <div className={classes.User} key={props.id}>
-        <div className={classes.name}>name: {props.name}</div>
-        <button onClick={() => props.handleFollowClick(props.id)}>
-          {props.isFollowed === 'active' ? 'Unfollow' : 'Follow'}
-        </button>
-        <input 
-          className={classes.hvrIconPop}
-          checked={props.isStared === 'active' ? true : false} 
-          onChange={() => props.handleStarClick(props.id)}
-          type='checkbox' 
-        />
-        <div>date: {props.date}</div>
-        <div>reading time: {props.readingTime}</div>
-        <img src={myImage} alt={props.name} />
+      <div className={classes.container} key={props.id}>
+        <img className={classes.imageContainer} src={myImage} alt={props.name} />
+        <div className={classes.contentContainer}>
+          <div className={classes.name}>name: {props.name}</div>
+          <button onClick={() => props.handleFollowClick(props.id)}>
+            {props.isFollowed === 'active' ? 'Unfollow' : 'Follow'}
+          </button>
+          <input 
+            className={classes.hvrIconPop}
+            checked={props.isStared === 'active' ? true : false} 
+            onChange={() => props.handleStarClick(props.id)}
+            type='checkbox' 
+          />
+          <div className={classes.date}>date: {props.date}</div>
+          <div className={classes.time}>reading time: {props.readingTime}</div>
+        </div>
       </div>
     )
 
