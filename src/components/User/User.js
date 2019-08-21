20d2936605736022dc887614db5ce33906175296
Saemie Chouchane <saemie@chouchane.com>
2019-08-21 07:48:47 +0100

    chore(formatting): Extract properties

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,34 +1,29 @@
-import React from 'react'
-import classes from './User.module.css'
-import myImage from '../../assets/images/avatar.png'
-import PropTypes from 'prop-types'
+import React from 'react';
+import classes from './User.module.css';
+import myImage from '../../assets/images/avatar.png';
+import PropTypes from 'prop-types';
 
-
-const User = props => {
-  
-    return(
-      <div className={classes.User} key={props.id}>
-        <div className={classes.name}>name: {props.name}</div>
-        <button onClick={() => props.handleFollowClick(props.id)}>
-          {props.isFollowed === 'active' ? 'Unfollow' : 'Follow'}
-        </button>
-        <input 
-          className={classes.hvrIconPop}
-          checked={props.isStared === 'active' ? true : false} 
-          onChange={() => props.handleStarClick(props.id)}
-          type='checkbox' 
-        />
-        <div>date: {props.date}</div>
-        <div>reading time: {props.readingTime}</div>
-        <img src={myImage} alt={props.name} />
-      </div>
-    )
-
-}
+const User = ({ id, name, date, isFollowed, isStared, handleFollowClick, handleStarClick, readingTime }) => {
+  return (
+    <div className={classes.User} key={id}>
+      <div className={classes.name}>name: {name}</div>
+      <button onClick={() => handleFollowClick(id)}>{isFollowed === 'active' ? 'Unfollow' : 'Follow'}</button>
+      <input
+        className={classes.hvrIconPop}
+        checked={isStared === 'active' ? true : false}
+        onChange={() => handleStarClick(id)}
+        type="checkbox"
+      />
+      <div>date: {date}</div>
+      <div>reading time: {readingTime}</div>
+      <img src={myImage} alt={name} />
+    </div>
+  );
+};
 
 User.propTypes = {
   handleFollowClick: PropTypes.func.isRequired,
-  handleStarClick: PropTypes.func.isRequired,
-}
+  handleStarClick: PropTypes.func.isRequired
+};
 
-export default User
+export default User;
