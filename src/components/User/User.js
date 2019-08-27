694879c68c108277b7dafa940d6290b4aff4d503
Mohamed Cherif <cherif.mohamed86@gmail.com>
2019-08-27 16:00:06 +0100

    finishing the styling

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -10,25 +10,32 @@ const User = ({ id, name, date, isFollowed, isStared, handleFollowClick, handleS
         <img className={classes.imageContainer} src={myImage} alt={name} />
         <div className={classes.contentContainer}>
           <div className={classes.contentContainerRow1}>
-            <div className={classes.name}>name: {name}</div>
+            <div className={classes.name}>{name}</div>
             <button onClick={() => handleFollowClick(id)}>
               {isFollowed === 'active' ? 'Unfollow' : 'Follow'}
             </button>
           </div>
           <div className={classes.contentContainerRow2}>
-            <div className={classes.date}>date: {date}</div>
-            <div className={classes.time}>reading time: {readingTime}</div>
-            <input 
-              className={classes.hvrIconPop}
-              checked={isStared === 'active' ? true : false} 
-              onChange={() => handleStarClick(id)}
-              type='checkbox' 
-            />
+            <div className={classes.date}>{date}</div>
+            <span>·</span>
+            <div className={classes.time}>{readingTime} min read</div>
+            <Icon />
           </div>
         </div>
       </div>
     )
 
+}
+
+function Icon() {
+  return (
+    <svg
+      style={{ marginTop: 4, padding: 4, width: 15, height: 15, fill: 'grey' }}
+      viewBox="0 0 15 15"
+    >
+      <path d="M7.44 2.32c.03-.1.09-.1.12 0l1.2 3.53a.29.29 0 00.26.2h3.88c.11 0 .13.04.04.1L9.8 8.33a.27.27 0 00-.1.29l1.2 3.53c.03.1-.01.13-.1.07l-3.14-2.18a.3.3 0 00-.32 0L4.2 12.22c-.1.06-.14.03-.1-.07l1.2-3.53a.27.27 0 00-.1-.3L2.06 6.16c-.1-.06-.07-.12.03-.12h3.89a.29.29 0 00.26-.19l1.2-3.52z" />
+    </svg>
+  );
 }
 
 User.propTypes = {
