9ee879c5ce4ba3df6f20b53ac1337dccf8bc86f3
Mohamed Cherif <cherif.mohamed86@gmail.com>
2019-08-28 18:00:22 +0100

    boilerplate for the Article Component

---
+++ src/components/Article/Article.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './Article.module.css';
+import myImage from '../../assets/images/article.jpeg'
+import PropTypes from 'prop-types'
+
+export default function Article() {
+  return(
+    <div>
+      something
+    </div>
+  )
+}
