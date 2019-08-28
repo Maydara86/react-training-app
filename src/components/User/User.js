b7bb206c375ccf99a085169e6aba53e15dcd3950
Mohamed Cherif <cherif.mohamed86@gmail.com>
2019-08-28 12:30:42 +0100

    add new style when we click on the star svg (unfinished test)

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 
 const User = ({ id, name, date, isFollowed, isStared, handleFollowClick, handleStarClick, readingTime }) => {
   
+  let starColor = isStared === 'active' ? 'gold' : 'grey'
     return(
       <div className={classes.container} key={id}>
         <img className={classes.imageContainer} src={myImage} alt={name} />
@@ -19,7 +20,7 @@ const User = ({ id, name, date, isFollowed, isStared, handleFollowClick, handleS
             <div className={classes.date}>{date}</div>
             <span>·</span>
             <div className={classes.time}>{readingTime} min read</div>
-            <Icon />
+            <Icon color={starColor} handleStarClick={handleStarClick} id={id} />
           </div>
         </div>
       </div>
@@ -27,11 +28,12 @@ const User = ({ id, name, date, isFollowed, isStared, handleFollowClick, handleS
 
 }
 
-function Icon() {
+export function Icon({color, handleStarClick, id}) {
   return (
     <svg
-      style={{ marginTop: 4, padding: 4, width: 15, height: 15, fill: 'grey' }}
+      style={{marginTop: 4, padding: 4, width: 15, height: 15, fill: color, cursor: 'pointer'}}
       viewBox="0 0 15 15"
+      onClick={() => handleStarClick(id)}
     >
       <path d="M7.44 2.32c.03-.1.09-.1.12 0l1.2 3.53a.29.29 0 00.26.2h3.88c.11 0 .13.04.04.1L9.8 8.33a.27.27 0 00-.1.29l1.2 3.53c.03.1-.01.13-.1.07l-3.14-2.18a.3.3 0 00-.32 0L4.2 12.22c-.1.06-.14.03-.1-.07l1.2-3.53a.27.27 0 00-.1-.3L2.06 6.16c-.1-.06-.07-.12.03-.12h3.89a.29.29 0 00.26-.19l1.2-3.52z" />
     </svg>
--- src/components/User/User.test.js
+++ src/components/User/User.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import TestRenderer from 'react-test-renderer';
 import {shallow, mount} from '../../enzyme';
 
-import User from './User';
+import User, {Icon} from './User';
 
 const user = {
   "id": "5d552d0058f193f2795fc814",
@@ -12,23 +13,7 @@ const user = {
   "readingTime": 20,
   "name": "Walton Morton",
   "date": "Aug 9"
-};
-
-it('renders correctly when there are no user', () => {
-  const tree = renderer.create(<User 
-    key={''}
-    id={''}
-    name={''}
-    date={''}
-    readingTime={''}
-    isStared={''}
-    isFollowed={''}
-    image={''}
-    handleFollowClick={() => {}}
-    handleStarClick={() => {}} 
-  />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
+}
 
 it('renders correctly when there is one user', () => {
 
@@ -49,12 +34,34 @@ it('renders correctly when there is one user', () => {
 
 
 
-
 it('when the follow button is clicked a callback is executed', () => {
+  const mockFollowClick = jest.fn();
+
+  const tree = renderer.create(<User 
+    key={user.id}
+    id={user.id}
+    name={user.name}
+    date={user.date}
+    readingTime={user.readingTime}
+    isStared={user.isStared}
+    isFollowed={user.isFollowed}
+    image={user.image}
+    handleFollowClick={mockFollowClick}
+    handleStarClick={() => {}}
+  />)
+
+  const button = tree.root.findByType('button');
+  button.props.onClick();
+  expect(mockFollowClick).toHaveBeenCalledWith('5d552d0058f193f2795fc814');  
+})
+
+
+
+it('Change the style of the star svg', () => {
   const mockFollowClick = jest.fn();
   const mockStarClick = jest.fn();
 
-  const tree = renderer.create(<User 
+  const testRenderer = TestRenderer.create(<User 
     key={user.id}
     id={user.id}
     name={user.name}
@@ -67,97 +74,26 @@ it('when the follow button is clicked a callback is executed', () => {
     handleStarClick={mockStarClick}
   />)
 
-  const button = tree.root.findByType('button');
-  // const input = tree.root.findByType('input');
+  const testInstance = testRenderer.root
+  // const button = testInstance.findByType('button')
+  // const icon = testInstance.findByType(Icon)
+  // testRenderer.root.findByType(Icon).onClick()
+  // testRenderer.root.findByType(Icon).onClick()
+  expect(testInstance.findByType(Icon).props.color).toBe('grey')
+  testInstance.findByType(Icon).simulate('click')
+  expect(testInstance.findByType(Icon).props.color).toBe('gold')
+
 
   // button.props.onClick();
   // expect(mockFollowClick).toHaveBeenCalled();
 
-  button.props.onClick();
-  expect(mockFollowClick).toHaveBeenCalledWith('5d552d0058f193f2795fc814');
-
-  // input.props.onChange();
-  // expect(mockStarClick).toHaveBeenCalled();
-
-  // input.props.onChange();
-  // expect(mockStarClick).toHaveBeenCalledWith('5d552d0058f193f2795fc814');
-  
-})
-
-it('when the follow button is clicked a callback is executed', () => {
-  const mockFollowClick = jest.fn();
-  const mockStarClick = jest.fn();
-
-  const tree = renderer.create(<User 
-    key={user.id}
-    id={user.id}
-    name={user.name}
-    date={user.date}
-    readingTime={user.readingTime}
-    isStared='active'
-    isFollowed={user.isFollowed}
-    image={user.image}
-    handleFollowClick={mockFollowClick}
-    handleStarClick={mockStarClick}
-  />)
-
-  const button = tree.root.findByType('button');
-  // const input = tree.root.findByType('input');
-
   // button.props.onClick();
-  // expect(mockFollowClick).toHaveBeenCalled();
-
-  button.props.onClick();
-  expect(mockFollowClick).toHaveBeenCalledWith('5d552d0058f193f2795fc814');
+  // expect(mockFollowClick).toHaveBeenCalledWith('5d552d0058f193f2795fc814');
 
   // input.props.onChange();
   // expect(mockStarClick).toHaveBeenCalled();
 
-  // input.props.onChange();
+  // icon.onClick();
   // expect(mockStarClick).toHaveBeenCalledWith('5d552d0058f193f2795fc814');
   
 })
-
-
-
-
-
-// describe('User Component', () => {
-//   it('clicking on the follow button will trigger the click handler', () => {
-//     const mockFollowHandler = jest.fn();
-//     const mockStarHandler = jest.fn();
-//     const wrapper = mount(<User 
-//       key={user.id}
-//       id={user.id}
-//       name={user.name}
-//       date={user.date}
-//       readingTime={user.readingTime}
-//       isStared={user.isStared}
-//       isFollowed={user.isFollowed}
-//       image={user.image}
-//       handleFollowClick={mockFollowHandler} 
-//       handleStarClick={mockStarHandler} 
-//     />)
-//     wrapper.find('button').simulate('click');
-//     expect(mockFollowHandler).toHaveBeenCalledWith('5d552d0058f193f2795fc814')
-//   })
-
-//   it('changing the star checkbox will trigger an onChange handler', () => {
-//     const mockFollowHandler = jest.fn();
-//     const mockStarHandler = jest.fn();
-//     const wrapper = mount(<User 
-//       key={user.id}
-//       id={user.id}
-//       name={user.name}
-//       date={user.date}
-//       readingTime={user.readingTime}
-//       isStared={user.isStared}
-//       isFollowed={user.isFollowed}
-//       image={user.image}
-//       handleFollowClick={mockFollowHandler} 
-//       handleStarClick={mockStarHandler} 
-//     />)
-//     wrapper.find('input').simulate('change');
-//     expect(mockStarHandler).toHaveBeenCalledWith('5d552d0058f193f2795fc814');
-//   })
-// });
