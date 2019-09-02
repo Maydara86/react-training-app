5a80ee7be70c1249b7c5a4721cd7e647ee0a5add
Mohamed Cherif <cherif.mohamed86@gmail.com>
2019-09-02 23:15:34 +0100

    test coverage 100%

--- src/components/Article/Article.test.js
+++ src/components/Article/Article.test.js
@@ -2,10 +2,7 @@ import React from 'react';
 import {shallow} from '../../enzyme';
 import Article, {ClapUnfilled, ClapFilled, BookmarkUnfilled, BookmarkFilled} from './Article'
 
-describe('Article', () => {
-    const mockHandleClapClick = jest.fn()
-    const mockHandleBookmarkClick = jest.fn()
-    const ClapComponent = shallow(<ClapFilled handleClapClick={mockHandleClapClick} />)
+describe('Article', () => {    
     const ArticleComponent = shallow(<Article 
         handleFollowClick={() => {}} 
         handleStarClick={() => {}} 
@@ -15,27 +12,56 @@ describe('Article', () => {
         expect(ArticleComponent).toMatchSnapshot()
     })
 
-    describe('checks bookmark', () => {
+    describe('checks bookmark ternary', () => {
         beforeEach(() => {
             ArticleComponent.setProps({bookmark: true})
         })
 
         it('renders BookmarkFilled component', () => {
-            expect(ArticleComponent.find('BookmarkFilled')).toBeTruthy()
+            expect(ArticleComponent.find('BookmarkFilled').exists()).toBeTruthy()
         })
     })
 
-    describe('when user did clap', () => {
+    describe('checks didClap ternary', () => {
         beforeEach(() => {
             ArticleComponent.setProps({didClap: true})
         })
         it('renders ClapFilled component', () => {
-            expect(ArticleComponent.find('ClapFilled')).toBeTruthy()
+            expect(ArticleComponent.find('ClapFilled').exists()).toBeTruthy()
+        })
+    })
+
+    describe('Bookmark Components', () => {
+        const mockHandleBookmarkClick = jest.fn()
+        const bookmarProps = {id: '5d66ae9445543ffea5167d5e', bookmarkClick: mockHandleBookmarkClick}
+        const Bookmark_filled = shallow(<BookmarkFilled {...bookmarProps} />)
+        const Bookmark_unfilled = shallow(<BookmarkUnfilled {...bookmarProps} />)
+        
+        it('when bookmarkClick is activated', () => {
+            Bookmark_filled.simulate('click')
+            expect(mockHandleBookmarkClick).toHaveBeenCalledWith(bookmarProps.id)
         })
 
-        // it('checks the handleClapClick method', () => {
-        //     ArticleComponent.find('ClapFilled').simulate('click')
-        //     expect(ClapComponent.mockHandleClapClick).toHaveBeenCalled()
-        // })
+        it('when bookmarkClick is deactivated', () => {
+            Bookmark_unfilled.simulate('click')
+            expect(mockHandleBookmarkClick).toHaveBeenCalledWith(bookmarProps.id)
+        })
+    })
+
+    describe('Clap Components', () => {
+        const mockHandleClapClick = jest.fn()
+        const clapProps = {id: '5d66ae9445543ffea5167d5e', handleClapClick: mockHandleClapClick}
+        const Clap_filled = shallow(<ClapFilled {...clapProps} />)
+        const Clap_unfilled = shallow(<ClapUnfilled {...clapProps} />)
+
+        it('when ClapUnfilled is clicked', () => {
+            Clap_unfilled.simulate('click')
+            expect(mockHandleClapClick).toHaveBeenCalledWith(clapProps.id)
+        })
+
+        it('when ClapFilled is clicked', () => {
+            Clap_filled.simulate('click')
+            expect(mockHandleClapClick).toHaveBeenCalledWith(clapProps.id)
+        })
     })
 })
