fa14c25f18e763aa4ac02badc14cb8fe9fc5ab81
Mohamed Cherif <cherif.mohamed86@gmail.com>
2019-09-03 15:36:50 +0100

    added 2 Snapshot test with & without props

--- src/App.test.js
+++ src/App.test.js
@@ -1,37 +1,41 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {shallow, mount} from './enzyme';
+import {shallow} from './enzyme';
+import renderer from 'react-test-renderer'
 import App from './App';
 
 jest.mock('./data/users-data.json')
-let {user} = require('./data/users-data.json')
 
+const AppComponent = shallow(<App />)
+const wrapper = AppComponent.instance()
 
 describe('App Component', () => {
-  it('testing clickFollowHandler method to have the expected effect on the state of the first user', () => {
-    const AppComponent = shallow(<App />)
-    const wrapper = AppComponent.instance()
+  describe('App Snapshots', () => {
+    it('renders correctly when the `state` is empty', () => {
+      expect(AppComponent).toMatchSnapshot()
+    })
+  
+    it('renders correctly when there is `state`', () => {
+      const tree = renderer.create(<App />).toJSON();
+      expect(tree).toMatchSnapshot();
+    })
+  })
+
+  it('testing clickFollowHandler method on the state\'s first user', () => {
     wrapper.clickFollowHandler('5d552d0058f193f2795fc814')
     expect(wrapper.state.users[0].isFollowed).toMatch('idle')
   })
 
-  it('testing clickFollowHandler method to have the expected effect on the state of the second user', () => {
-    const AppComponent = shallow(<App />)
-    const wrapper = AppComponent.instance()
+  it('testing clickFollowHandler on the state\'s second user', () => {
     wrapper.clickFollowHandler('5d552d00b20b141dff10d2a2')
     expect(wrapper.state.users[1].isFollowed).toMatch('active')
   })
 
   it('Testing clickStarHandler method to have the expected effect on the state of the first user', () => {
-    const AppComponent = shallow(<App />)
-    const wrapper = AppComponent.instance()
     wrapper.clickStarHandler('5d552d0058f193f2795fc814')
     expect(wrapper.state.users[0].isStarred).toEqual('active')
   })
 
   it('calling the clickStarHandler method from App Component has the expected effect on the state of the second user', () => {
-    const AppComponent = shallow(<App />)
-    const wrapper = AppComponent.instance()
     wrapper.clickStarHandler('5d552d00b20b141dff10d2a2')
     expect(wrapper.state.users[1].isStarred).toEqual('idle')
   })
