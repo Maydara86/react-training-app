a291f6daae5edb3916172c8d905d16cbc9ea8989
Mohamed Cherif <cherif.mohamed86@gmail.com>
2019-09-06 15:48:54 +0100

    various snapshot test for ArticleGrid

--- src/components/ArticleGrid/ArticleGrid.test.js
+++ src/components/ArticleGrid/ArticleGrid.test.js
@@ -1,16 +1,31 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
-import { mount } from '../../enzyme'
+import toJson from 'enzyme-to-json'
+import { mount, shallow, render } from '../../enzyme'
 import ArticleGrid from './ArticleGrid'
 
-jest.mock('../Article/Article', () => () => 'Article')
-jest.mock('../User/User', () => () => 'User')
-const wrapper = mount(<ArticleGrid />)
+jest.mock('../Article/Article', () => 'Article')
+jest.mock('../User/User', () => 'User')
+jest.mock('../Svg/Svg', () => 'ClapFilled')
+jest.mock('../Svg/Svg', () => 'ClapUnfilled')
+jest.mock('../Svg/Svg', () => 'BookmarkFilled')
+jest.mock('../Svg/Svg', () => 'BookmarkUnfilled')
+jest.mock('../Svg/Svg', () => 'Star')
 
 describe('ArticleGrid', () => {
-  fit('renders corrctly', () => {
-    // wrapper.toJSON()
-    expect(wrapper.children()).toMatchSnapshot()
+  xit('renders corrctly using mount', () => {
+    const wrapper = mount(<ArticleGrid />)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+  xit('renders corrctly using shallow', () => {
+    const articleGrid = shallow(<ArticleGrid />)
+    expect(articleGrid).toMatchSnapshot()
+  })
+
+  xit('renders corrctly using enzym render', () => {
+    const articleGridRender = render.create(<ArticleGrid />)
+    expect(articleGridRender).toMatchSnapshot()
   })
 
   xit('renders correctly using jest renderer', () => {
