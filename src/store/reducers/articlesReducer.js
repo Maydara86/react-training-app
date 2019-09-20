2ec3c738a0ac2d1037ec17b49902a7e82038134e
Mohamed Cherif <cherif.mohamed86@gmail.com>
2019-09-20 13:37:58 +0100

    reducers manages the state in an immutable way

--- src/store/reducers/articlesReducer.js
+++ src/store/reducers/articlesReducer.js
@@ -3,21 +3,14 @@ import { TOGGLE_BOOKMARK_ICON, TOGGLE_CLAP_ICON } from '../actions/articlesActio
 const articlesReducer = (state = [], { type, payload }) => {
   switch (type) {
     case TOGGLE_BOOKMARK_ICON:
-      return state.map(article => {
-        if (article.id === payload) {
-          article.bookmark = !article.bookmark
-        }
-        return article
-      })
+      return state.map(article =>
+        article.id === payload ? { ...article, bookmark: !article.bookmark } : article
+      )
 
     case TOGGLE_CLAP_ICON:
-      return state.map(article => {
-        if (article.id === payload) {
-          article.didClap = true
-          article.claps += 1
-        }
-        return article
-      })
+      return state.map(article =>
+        article.id === payload ? { ...article, didClap: true, claps: article.claps + 1 } : article
+      )
 
     default:
       return state
--- src/store/reducers/usersReducer.js
+++ src/store/reducers/usersReducer.js
@@ -3,20 +3,14 @@ import { TOGGLE_STAR_ICON, CLICK_FOLLOW_BUTTON } from '../actions/usersAction'
 const usersReducer = (state = [], { type, payload }) => {
   switch (type) {
     case TOGGLE_STAR_ICON:
-      return state.map(user => {
-        if (user.id === payload) {
-          user.isStarred = !user.isStarred
-        }
-        return user
-      })
+      return state.map(user =>
+        user.id === payload ? { ...user, isStarred: !user.isStarred } : user
+      )
 
     case CLICK_FOLLOW_BUTTON:
-      return state.map(user => {
-        if (user.id === payload) {
-          user.isFollowed = !user.isFollowed
-        }
-        return user
-      })
+      return state.map(user =>
+        user.id === payload ? { ...user, isFollowed: !user.isFollowed } : user
+      )
 
     default:
       return state
